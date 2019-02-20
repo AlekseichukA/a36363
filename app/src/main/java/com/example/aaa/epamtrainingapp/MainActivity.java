7310e4bc776a1dda32a2c17d5f602d110184d0e6
AlekseichukA <alekseichuk4@gmail.com>
2019-02-20 20:43:35 +0300

    first commit

--- app/src/main/java/com/example/aaa/epamtrainingapp/MainActivity.java
+++ app/src/main/java/com/example/aaa/epamtrainingapp/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText searchField;
     private Button button;
     private TextView result;
-
+    //kgkgkjgkjgkjgj
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
